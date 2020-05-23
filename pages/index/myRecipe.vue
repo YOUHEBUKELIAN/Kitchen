6b01f4973ed7e1b698748d21f71627b749023f99
qiuqiuqiu123 <34887652+qiuqiuqiu123@users.noreply.github.com>
2020-05-23 19:06:09 +0800

    2020/5/23 提交myRecipe

---
+++ pages/index/myRecipe.vue
@@ -0,0 +1,47 @@
+<template name="page2">
+	<view class="pageView bg-purple">Page 1</view>
+</template>
+
+<script>
+	export default {
+		name: "page2",
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		onShow() {
+			console.log(e)
+			uni.switchTab({
+				url: "../index/index",
+				"open-type": "switchTab"
+			})
+		},
+		onLoad(e) {
+			console.log(e.id);
+			uni.request({
+				url: 'http://pope.utools.club/findMyRecipe',
+				data: {
+					nickname: e.nickname
+				},
+				method: 'POST',
+				header: {
+					'content-type': 'application/x-www-form-urlencoded',
+				},
+				success: (ret) => {
+					console.log(ret.data)
+					this.list = ret.data.data
+				}
+			});
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
