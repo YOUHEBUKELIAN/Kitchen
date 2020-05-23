44f62bd2c3ccd71d4221da4bd633cba2cb7a0b6c
qiuqiuqiu123 <34887652+qiuqiuqiu123@users.noreply.github.com>
2020-05-23 19:34:28 +0800

    2020/5/23 解决myRecipe语法不正规

--- pages/index/myRecipe.vue
+++ pages/index/myRecipe.vue
@@ -1,5 +1,24 @@
 <template name="page2">
-	<view class="pageView bg-purple">Page 1</view>
+	<view class="pageView bg-purple">
+			<view v-for="item in items" :key="item.name">
+				<view class="uni-padding-wrap uni-common-mt" style="background-color:#808080" >
+					<view class="uni-flex uni-row">
+						<view class="text uni-flex" style="width: 200rpx;height: 220rpx;-webkit-justify-content: center;justify-content: center;-webkit-align-items: center;align-items: center;">
+							<image :src="item.cover" style="width: 150rpx;height: 150rpx;"></image>
+						</view>
+						<view class="uni-flex uni-column" style="-webkit-flex: 1;flex: 1;-webkit-justify-content: space-between;justify-content: space-between;">
+							<view class="text" style="height: 120rpx;text-align: left;padding-left: 20rpx;padding-top: 10rpx;">
+								{{item.name}}
+							</view>
+							<view class="uni-flex uni-row">
+								<view class="text" style="-webkit-flex: 1;flex: 1;">{{item.collectionNumber}}</view>
+								<view class="text" style="-webkit-flex: 1;flex: 1;">{{item.browseNumber}}</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 </template>
 
 <script>
@@ -7,21 +26,24 @@
 		name: "page2",
 		data() {
 			return {
-
+				items:[{
+					cover:"123",
+					name:"465",
+					collectionNumber:7,
+					browseNumber:8
+				}]
 			}
 		},
 		onLoad() {
 
 		},
 		onShow() {
-			console.log(e)
 			uni.switchTab({
 				url: "../index/index",
 				"open-type": "switchTab"
 			})
 		},
 		onLoad(e) {
-			console.log(e.id);
 			uni.request({
 				url: 'http://pope.utools.club/findMyRecipe',
 				data: {
@@ -44,4 +66,22 @@
 </script>
 
 <style>
+	.example {
+		padding: 0 15px 15px;
+	}
+	
+	.example-info {
+		padding: 15px;
+		color: #3b4144;
+		background: #ffffff;
+	}
+	
+	.example-body {
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		padding: 0;
+		font-size: 14px;
+		background-color: #ffffff;
+	}
 </style>
